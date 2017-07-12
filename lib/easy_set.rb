ef3ea68641206d1957b2d63996a69b99059a0b41
Martin Becker <mbeckerwork@gmail.com>
2017-07-12 12:02:54 +0100

    fixes indentation

--- lib/easy_set.rb
+++ lib/easy_set.rb
@@ -2,47 +2,43 @@ require_relative "./easy_set/version"
 
 require 'yaml'
 module Easy_Set
+    module Singleton_Methods
 
-	module Singleton_Methods
-
-		def [](key)
-			@store[key]
-		end
-
-		def display
-			@store.inspect
-		end
-
-		def generate_rand_string(length=nil)
-			o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
-	    return (0...(length||32)).map { o[rand(o.length)] }.join
-		end
-
-		def load_config_from path
-			@store[:config] = YAML.load_file(path)
-		end
-
-    def to_s
-      @store.each do |key,value|
-        puts "#{key}: #{value.to_s}"
+      def [](key)
+        @store[key]
       end
-      puts ""
-    end
 
-		private
-			def set key, value
-				@store[key] = value
-			end
+      def display
+        @store.inspect
+      end
 
-	end
+      def generate_rand_string(length=nil)
+        o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+        return (0...(length||32)).map { o[rand(o.length)] }.join
+      end
+
+      def load_config_from path
+        @store[:config] = YAML.load_file(path)
+      end
+
+      def to_s
+        @store.each do |key,value|
+          puts "#{key}: #{value.to_s}"
+        end
+        puts ""
+      end
+
+      private
+        def set key, value
+          @store[key] = value
+        end
+
+      end
 
 
-	def self.included(child)
-		child.instance_variable_set(:@store, {})
-		child.extend(Singleton_Methods)
-	end
+      def self.included(child)
+        child.instance_variable_set(:@store, {})
+        child.extend(Singleton_Methods)
+      end
 
-	def self.includes_hooks?
-    	%w(inherited)
-	end
 end
